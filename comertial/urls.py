149a2e749f654b60280518a27dd3992851262c52
fernandobusta <bustafernando@icloud.com>
2023-03-08 21:54:11 +0000

    Completely changed the theme, settings for production also set

--- comertial/urls.py
+++ comertial/urls.py
@@ -2,16 +2,16 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-   path('index', index, name="index"),
-   path('contact', contact, name="contact"),
-   path('riddle', riddle, name="riddle"),
-   path('mineswp', mineswp, name="mineswp"),
-   path('', homepage, name="homepage"),
-   path('simple', simple, name="simple"),
-   path('completed', completed, name="completed"),
    
    # Form
-   path('questions', questionsView, name='questions')
+   path('questions', questionsView, name='questions'),
+
+   # For the new template
+   path('', index, name="index"),
+   path('mines', mines, name="mines"),
+   path('about', about, name="about"),
+   path('test', test, name="test"),
+   path('contact', contact, name="contact"),
 
 ]
 urlpatterns += [
--- comertial/views.py
+++ comertial/views.py
@@ -3,33 +3,7 @@ from django.contrib.auth.decorators import login_required
 from .models import *
 from .forms import *
 
-def index(request):
-    # Number of visits to this view, as counted in the session variable.
-    num_visits = request.session.get('num_visits', 0)
-    request.session['num_visits'] = num_visits + 1
 
-    context = {
-        'num_visits': num_visits,
-    }
-    return render(request, 'comertial/index.html', context=context)
-
-def contact(request):
-    return render(request, 'comertial/contact.html')
-
-def riddle(request):
-    return render(request, 'comertial/riddle.html')
-
-def mineswp(request):
-    return render(request, 'comertial/mineswp.html')
-
-
-# Comertial page -> Info about project
-def homepage(request):
-    return render(request, 'comertial/homepage.html')
-
-def simple(request):
-    """Simple view for testing base.html"""
-    return render(request, 'comertial/simple.html')
 
 def questionsView(request):
     if request.method == "POST":
@@ -38,12 +12,30 @@ def questionsView(request):
         form = QuestionForm(request.POST)
         if form.is_valid():
             quest = form.save()
-            return redirect('completed')
+            return redirect('index')
         else:
-            return render(request, 'comertial/questions.html', {'form':form})
+            return render(request, 'comertial2/questions.html', {'form':form})
     else:
         form = QuestionForm()
-        return render(request, 'comertial/questions.html', {'form':form})
+        return render(request, 'comertial2/questions.html', {'form':form})
     
 def completed(request):
-    return render(request, 'comertial/completed.html')
+    return render(request, 'comertial2/completed.html')
+
+
+
+# This is for the new template ==========================================================
+def index(request):
+    return render(request, 'comertial2/index.html')
+
+def mines(request):
+    return render(request, 'comertial2/mines.html')
+
+def about(request):
+    return render(request, 'comertial2/about.html')
+
+def contact(request):
+    return render(request, 'comertial2/contact.html')
+
+def test(request):
+    return render(request, 'comertial2/test.html')
