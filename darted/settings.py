677901de61f0a0c979044f0eb7e0d9a7f0bef447
fernandobusta <bustafernando@icloud.com>
2023-03-08 15:01:04 +0000

    Update ALLOWED_HOSTS and CSRF_TRUSTED_ORIGINS with site URL

--- darted/settings.py
+++ darted/settings.py
@@ -38,7 +38,7 @@ DEBUG = True
 
 ## For example, for a site URL at 'web-production-3640.up.railway.app'
 ## (replace the string below with your own site URL):
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['web-production-c6bf.up.railway.app', '127.0.0.1']
 # During development, you can instead set just the base URL
 # (you might decide to change the site a few times).
 # ALLOWED_HOSTS = ['.railway.com','127.0.0.1']
@@ -171,6 +171,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ## For example, for a site URL is at 'web-production-3640.up.railway.app'
 ## (replace the string below with your own site URL):
-
+CSRF_TRUSTED_ORIGINS = ['https://web-production-c6bf.up.railway.app']
 # During development/for this tutorial you can instead set just the base URL
 # CSRF_TRUSTED_ORIGINS = ['https://*.railway.app']
