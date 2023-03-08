be880f378f1222e1dd6229f2385c0334a6ffb70e
fernandobusta <bustafernando@icloud.com>
2023-03-08 14:04:17 +0000

    Change in setttings for postgres

--- darted/settings.py
+++ darted/settings.py
@@ -13,14 +13,14 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv(os.path.join(BASE_DIR, ".env"))
+#load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 
 DATABASE_URL = os.getenv("DATABASE_URL")
