c69498c27b363301c1bea1b5a019a6b54541a840
fernandobusta <bustafernando@icloud.com>
2023-03-09 13:08:01 +0000

    Change in Urls commenting a line for static serving -> hope to fix deployment

--- darted/urls.py
+++ darted/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
 ]
 
 # Use static() to add url mapping to serve static files during development (only)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 # Add URL maps to redirect the base URL to our application
 urlpatterns += [
